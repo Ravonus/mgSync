dabe3f5ff5cbe690e82215cf2bf154b73aa74cd3
Chad Koslovsky <chad@technomancyit.com>
2019-02-17 11:52:32 -0700

    Added dspConf fix

--- config/scripts/dspConf.js
+++ config/scripts/dspConf.js
@@ -8,7 +8,7 @@ let found = {};
 
 //fs.readDirSync = promisify(fs.readdir);
 try {
-if(config.dsp['conf-dir'].substring(config.dsp['conf-dir'].length -1) !=='/' || config.dsp['conf-dir'].substring(config.dsp['conf-dir'].length -1) !=='\\') config.dsp['conf-dir']+'/';
+if(config.dsp['conf-dir'].substring(config.dsp['conf-dir'].length -1) !=='/' || config.dsp['conf-dir'].substring(config.dsp['conf-dir'].length -1) !=='\\') config.dsp['conf-dir'] = config.dsp['conf-dir']+'/';
 var files = fs.readdirSync(config.dsp['conf-dir']);
 files.forEach((file, index) => {
 
