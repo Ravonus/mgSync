25df468947f9894b3c97d431cf9a4bc4dcc86e40
Chad Koslovsky <chad@technomancyit.com>
2019-02-20 23:54:20 -0700

    Added start to authenitication database method.

--- controllers/sequelize.js
+++ controllers/sequelize.js
@@ -1,4 +1,6 @@
-const Sequelize = require('sequelize');
+
+const config = require('../config/scripts/config'),
+  Sequelize = require('sequelize');
 module.exports = sequelize = new Sequelize(config.dsp['mysql-db'], config.dsp['mysql-user'], config.dsp['mysql-password'], {
   host: config.dsp['mysql-host'],
   dialect: 'mysql',
@@ -11,16 +13,41 @@ module.exports = sequelize = new Sequelize(config.dsp['mysql-db'], config.dsp['m
 
   // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
   operatorsAliases: false, logging: false
-  
-});
 
+});
 
 sequelize
   .authenticate()
   .then(() => {
-    
+    if (config.express.signUp) sequelizeMgNewFunc();
   })
   .catch(err => {
-    if(err.original.sqlMessage) log({err:err.original.sqlMessage, type:'error'});
+    console.log(err);
+    if (err.original.sqlMessage) log({ err: err.original.sqlMessage, type: 'error' });
     log(`reachMysql`, [err.original.hostname])
   });
+
+function sequelizeMgNewFunc() {
+  const sequelizeMgNew = new Sequelize("", config.dsp['mysql-user'], config.dsp['mysql-password'], {
+    host: config.dsp['mysql-host'],
+    dialect: "mysql",
+    operatorsAliases: false, logging: false
+  });
+  sequelizeMgNew.query("CREATE DATABASE `mgSync`;").then(data => {
+    //log here
+    sequelizeMgFunc()
+  }).catch(e => { sequelizeMgFunc() });
+}
+
+function sequelizeMgFunc() {
+  const sequelizeMg = new Sequelize("mgSync", config.dsp['mysql-user'], config.dsp['mysql-password'], {
+    host: config.dsp['mysql-host'],
+    dialect: "mysql",
+    operatorsAliases: false, logging: false
+  });
+
+  sequelizeMg.query("CREATE TABLE IF NOT EXISTS `users` (`accid` int(11) NULL, `username` varchar(16) NULL, `email` varchar(200) NULL, `verified` int(1) NOT NULL default '0', `permissions` INT(8) NOT NULL default '1', `groups` INT(11) null, `status` INT(11) NOT NULL default '1');")
+    .then(data => {
+
+    })
+}
